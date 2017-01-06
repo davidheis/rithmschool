35a382a0bb9f26778c1367ad9877ce8e584034e0
David Heisner <davidheis@hotmail.com>
2017-01-06 21:05:00 +0100

    testing my git skills

--- rithmschool.comexercises.js
+++ rithmschool.comexercises.js
@@ -2,6 +2,7 @@
 // to show you how stupid it is to buy a lottery ticket, just try 
 // and hit this jackpot, play as much as you want. This is 1 in 10 000 you could change it to 
 // reflect a jackpot which might be something like 1 in 14 000 000
+// :)
 
 // function guess(x){
 // 	var limit = 10001
