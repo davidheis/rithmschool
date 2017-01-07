4522bc5da32e3c16c42a7916f477641f1bd9a4a9
David Heisner <davidheis@hotmail.com>
2017-01-07 15:58:02 +0100

    add more functions

--- rithmschoolexercises.js
+++ rithmschoolexercises.js
@@ -405,9 +405,18 @@ function addSpeaker(speakerName){
 }
 
 // Write a function addLanguage that adds a language to the languages object. The language object you add should have a key with the name of the language and the value of another object with a key of "hello" and a value of however you spell "hello" in the language you add.
-
+function addLanguage(newLanguage, sayHello){
+	nestedObject.data.languages[newLanguage] = {
+		hello: sayHello
+	}
+}
 // Write a function addCountry that adds a European country to the countries object (inside of the continents object, inside of the countries object). The country you add should be an object with the key as name of the country and the value as an object with the keys of "capital" and "population" and their respective values.
-
+function addCountry (countryName, capital, population){
+	nestedObject.data.continents.europe.countries[countryName] = {
+		capital: capital,
+		population: population
+	}
+}
 
 
 
